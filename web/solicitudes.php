7abebead684cffa02c89482eb9f097b77e65f0d4
HmarquezF <121980776+hmarquezf@users.noreply.github.com>
2023-11-26 13:12:18 -0500

    solicitudes solicitudes

--- web/solicitudes.php
+++ web/solicitudes.php
@@ -47,6 +47,29 @@
             </tr>
         </thead>
         <tbody>
+        
+        <?php
+        if(isset($_POST['filtroForm'])){
+         require('config.php');
+
+         $filtroID = $_GET['filtroID'];
+         $sql = "SELECT reportes.reportes as IDReporte, clientes.nombre as Nombres, reportes.contactado as Contactado, vendedores.Nombre as Asesor_seleccionado, reportes.detalle as Detalle FROM reportes, clientes, vendedores WHERE reportes.idCliente = '$filtroID' and reportes.idCliente = clientes.id and reportes.idAsesor = vendedores.referencia";
+
+         $resultado = mysqli_query($db_link, $sql);
+
+         while ($fila = mysqli_fetch_array($resultado)) {
+            echo "<tr>"
+                ."<td>" . $fila['IDReporte'] . "</td>"
+                ."<td>" . $fila['Nombres'] . "</td>"
+                ."<td>" . $fila['Contactado'] . "</td>"
+                ."<td>" . $fila['Asesor_seleccionado'] . "</td>"
+                ."<td>" . $fila['Detalle'] . "</td>"
+            . "</tr>";
+         }
+
+         mysqli_close($db_link);
+        }
+?>
         </tbody>
     </table>
 
